2dd5540bbdcb4538724546b150994f153d4d116d
Moto ITO <motonari.ito@gmail.com>
2025-11-05 10:07:37 -0800

    Stop iteration when converged.

--- Sources/Gridworld/Gridworld.swift
+++ Sources/Gridworld/Gridworld.swift
@@ -8,22 +8,29 @@ struct Gridworld {
         let grid = Grid()
         var agent = Agent(grid: grid, discount: discount)
 
-        for _ in 0..<1_000 {
+        var delta: Double = 0
+        repeat {
+            delta = 0
             for y in 0..<grid.height {
                 for x in 0..<grid.width {
                     let state = State(x: x, y: y)
 
-                    var stateValue = 0.0
+                    var newStateValue = 0.0
                     for action in Action.allCases {
                         let (nextState, reward) = grid.reward(state: state, action: action)
-                        stateValue += reward + discount * agent.value(of: nextState)
+                        newStateValue +=
+                            (reward + discount * agent.value(of: nextState))
+                            / Double(Action.allCases.count)
                     }
+
+                    delta = max(delta, abs(agent.value(of: state) - newStateValue))
                     agent.update(
                         state: state,
-                        value: stateValue / Double(Action.allCases.count))
+                        value: newStateValue)
+
                 }
             }
-        }
+        } while delta > 0
 
         agent.dumpStateValues()
     }
