55f121aef2e029148d678690505390d363fa6f80
Moto ITO <motonari.ito@gmail.com>
2025-11-05 10:00:41 -0800

    Use in-place iterative policy evaluation.

--- Sources/Gridworld/Gridworld.swift
+++ Sources/Gridworld/Gridworld.swift
@@ -5,24 +5,24 @@
 struct Gridworld {
     static func main() {
         let discount = 0.9
-        var state = State(x: 2, y: 2)
         let grid = Grid()
         var agent = Agent(grid: grid, discount: discount)
 
-        for _ in 0..<5_000 {
+        for _ in 0..<1_000 {
+            for y in 0..<grid.height {
+                for x in 0..<grid.width {
+                    let state = State(x: x, y: y)
 
-            var stateValue = 0.0
-            for action in Action.allCases {
-                let (nextState, reward) = grid.reward(state: state, action: action)
-                stateValue += reward + discount * agent.value(of: nextState)
+                    var stateValue = 0.0
+                    for action in Action.allCases {
+                        let (nextState, reward) = grid.reward(state: state, action: action)
+                        stateValue += reward + discount * agent.value(of: nextState)
+                    }
+                    agent.update(
+                        state: state,
+                        value: stateValue / Double(Action.allCases.count))
+                }
             }
-            agent.update(
-                state: state,
-                value: stateValue / Double(Action.allCases.count))
-
-            let action = Action.allCases.randomElement()!
-            let (nextState, _) = grid.reward(state: state, action: action)
-            state = nextState
         }
 
         agent.dumpStateValues()
